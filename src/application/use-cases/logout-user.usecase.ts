121a61805c6d6c0c694d45f7f758750a05163a81
SRT2801 <sahirruiz4@gmail.com>
2025-03-22 17:37:36 -0500

    [ADD] Implementar caso de uso para cerrar sesión de usuario

---
+++ src/application/use-cases/logout-user.usecase.ts
@@ -0,0 +1,21 @@
+import { InvalidatedToken } from 'src/domain/entities/invalidated-token.entity';
+import { InvalidatedTokenRepository } from 'src/domain/repositories/invalidated-token.repository';
+import * as jwt from 'jsonwebtoken';
+
+export class LogoutUserUseCase {
+  constructor(
+    private readonly invalidatedTokenRepository: InvalidatedTokenRepository,
+  ) {}
+
+  async execute(token: string): Promise<void> {
+    const decoded = jwt.decode(token) as { exp: number };
+    if (!decoded || !decoded.exp) {
+      throw new Error('Formato de token inválido');
+    }
+
+    const expiresAt = new Date(decoded.exp * 1000);
+
+    const invalidatedToken = new InvalidatedToken(token, expiresAt);
+    await this.invalidatedTokenRepository.save(invalidatedToken);
+  }
+}
