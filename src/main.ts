ae4a38add109dd613447032c3ad5abc798635aad
SRT2801 <sahirruiz4@gmail.com>
2025-03-22 19:42:44 -0500

    [ADD] Establecer prefijo global 'api' para las rutas

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.setGlobalPrefix('api');
+  
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
